239f61340735429332504fe0fe30e3d158385241
Rita Bennett-Chew and Reese Glasscock <rita@>
2018-07-16 10:41:54 -0700

    pass test 2 for class Employee

---
+++ spec/employee_spec.rb
@@ -0,0 +1,11 @@
+require('spec_helper')
+
+describe (Employee) do
+  describe('#division') do
+    it "identifies the division for a specific employee" do
+      division = Division.create({:name => 'Sales'})
+      employee = Employee.create({:name => 'Jeff', :division_id => division.id})
+      expect(employee.division).to(eq(division))
+    end
+  end
+end
